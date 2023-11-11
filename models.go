ff449600334f8d259f29b9f4cc5591722843d758
Drew Weymouth <dweymouth@gmail.com>
2023-11-11 10:01:40 -0800

    update models

--- models.go
+++ models.go
@@ -25,21 +25,31 @@ type NameID struct {
 
 type Images struct {
 	Primary string `json:"Primary"`
+	Disc    string `json:"Disc"`
+}
+
+type MediaSource struct {
+	Bitrate   int    `json:"Bitrate"`
+	Container string `json:"Container"`
+	Path      string `json:"Path"`
+	Size      int    `json:"Size"`
 }
 
 type Song struct {
-	Name           string   `json:"Name"`
-	Id             string   `json:"Id"`
-	Duration       int64    `json:"RunTimeTicks"`
-	ProductionYear int      `json:"ProductionYear"`
-	IndexNumber    int      `json:"IndexNumber"`
-	Type           string   `json:"Type"`
-	AlbumID        string   `json:"AlbumId"`
-	Album          string   `json:"Album"`
-	DiscNumber     int      `json:"ParentIndexNumber"`
-	Artists        []NameID `json:"ArtistItems"`
-
-	UserData UserData `json:"UserData"`
+	Name           string        `json:"Name"`
+	Id             string        `json:"Id"`
+	RunTimeTicks   int64         `json:"RunTimeTicks"`
+	ProductionYear int           `json:"ProductionYear"`
+	DateCreated    string        `json:"DateCreated"`
+	IndexNumber    int           `json:"IndexNumber"`
+	Type           string        `json:"Type"`
+	AlbumID        string        `json:"AlbumId"`
+	Album          string        `json:"Album"`
+	DiscNumber     int           `json:"ParentIndexNumber"`
+	Artists        []NameID      `json:"ArtistItems"`
+	ImageTags      Images        `json:"ImageTags"`
+	MediaSources   []MediaSource `json:"MediaSources"`
+	UserData       UserData      `json:"UserData"`
 }
 
 type songs struct {
@@ -48,13 +58,14 @@ type songs struct {
 }
 
 type Artist struct {
-	Name          string   `json:"Name"`
-	ID            string   `json:"Id"`
-	TotalDuration int64    `json:"RunTimeTicks"`
-	Type          string   `json:"Type"`
-	TotalSongs    int      `json:"SongCount"`
-	TotalAlbums   int      `json:"AlbumCount"`
-	UserData      UserData `json:"UserData"`
+	Name         string   `json:"Name"`
+	ID           string   `json:"Id"`
+	RunTimeTicks int64    `json:"RunTimeTicks"`
+	Type         string   `json:"Type"`
+	SongCount    int      `json:"SongCount"`
+	AlbumCount   int      `json:"AlbumCount"`
+	UserData     UserData `json:"UserData"`
+	ImageTags    Images   `json:"ImageTags"`
 }
 
 type artists struct {
@@ -63,16 +74,17 @@ type artists struct {
 }
 
 type Album struct {
-	Name      string   `json:"Name"`
-	ID        string   `json:"Id"`
-	Duration  int64    `json:"RunTimeTicks"`
-	Year      int      `json:"ProductionYear"`
-	Type      string   `json:"Type"`
-	Artists   []NameID `json:"AlbumArtists"`
-	Overview  string   `json:"Overview"`
-	Genres    []string `json:"Genres"`
-	ImageTags Images   `json:"ImageTags"`
-	UserData  UserData `json:"UserData"`
+	Name         string   `json:"Name"`
+	ID           string   `json:"Id"`
+	RunTimeTicks int64    `json:"RunTimeTicks"`
+	Year         int      `json:"ProductionYear"`
+	DateCreated  string   `json:"DateCreated"`
+	Type         string   `json:"Type"`
+	Artists      []NameID `json:"AlbumArtists"`
+	Overview     string   `json:"Overview"`
+	Genres       []string `json:"Genres"`
+	ImageTags    Images   `json:"ImageTags"`
+	UserData     UserData `json:"UserData"`
 }
 
 type albums struct {
@@ -81,13 +93,17 @@ type albums struct {
 }
 
 type Playlist struct {
-	Name      string   `json:"Name"`
-	ID        string   `json:"Id"`
-	Genres    []string `json:"Genres"`
-	Duration  int64    `json:"RunTimeTicks"`
-	Type      string   `json:"Type"`
-	MediaType string   `json:"MediaType"`
-	SongCount int      `json:"ChildCount"`
+	Name               string   `json:"Name"`
+	ID                 string   `json:"Id"`
+	Overview           string   `json:"Overview"`
+	DateCreated        string   `json:"DateCreated"`
+	DateLastMediaAdded string   `json:"DateLastMediaAdded"`
+	Genres             []string `json:"Genres"`
+	RunTimeTicks       int64    `json:"RunTimeTicks"`
+	Type               string   `json:"Type"`
+	MediaType          string   `json:"MediaType"`
+	ImageTags          Images   `json:"ImageTags"`
+	SongCount          int      `json:"ChildCount"`
 }
 
 type playlists struct {
