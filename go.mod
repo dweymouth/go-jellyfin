d93b76137091dcd5989bcb30a18e5f9bd47f2d56
Drew Weymouth <dweymouth@gmail.com>
2023-11-11 17:02:53 -0800

    add GetItemImage with image.Image decoding

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dweymouth/go-jellyfin
 
 go 1.17
+
+require golang.org/x/image v0.14.0
--- retrieval.go
+++ retrieval.go
@@ -2,16 +2,37 @@ package jellyfin
 
 import (
 	"fmt"
+	"image"
 	"io"
 	"strconv"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
+	_ "golang.org/x/image/webp"
 )
 
-func (c *Client) GetItemImage(itemID, imageTag string, size, quality int) (io.ReadCloser, error) {
+func (c *Client) GetItemImageBinary(itemID, imageTag string, size, quality int) (io.ReadCloser, error) {
 	path := fmt.Sprintf("/Items/%s/Images/%s", itemID, imageTag)
 	params := c.defaultParams()
 	params["width"] = strconv.Itoa(size)
 	params["quality"] = strconv.Itoa(quality)
 	return c.get(path, params)
+}
+
+func (c *Client) GetItemImage(itemID, imageTag string, size, quality int) (image.Image, error) {
+	body, err := c.GetItemImageBinary(itemID, imageTag, size, quality)
+	if err != nil {
+		return nil, err
+	}
+	defer body.Close()
+
+	image, _, err := image.Decode(body)
+	if err != nil {
+		return nil, err
+	}
+	return image, nil
 }
 
 func (c *Client) GetStreamURL(id string) (string, error) {
