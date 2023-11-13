8cce8cd32c5995fe1ae3646e7d44f4ef1543fa21
Drew Weymouth <dweymouth@gmail.com>
2023-11-12 18:38:51 -0800

    add playlist + few other endpoints

--- playlist.go
+++ playlist.go
@@ -2,8 +2,8 @@ package jellyfin
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
+	"strings"
 )
 
 type createPlaylistBody struct {
@@ -47,8 +47,56 @@ func (c *Client) GetPlaylistSongs(playlistID string) ([]*Song, error) {
 	return c.parseSongs(resp)
 }
 
-func (c *Client) AddPlaylistTracks(playlistID string, trackIDs []string) error {
-	return errors.New("not implemented")
+type updatePlaylistBody struct {
+	Name     string `json:"Name"`
+	Overview string `json:"Overview"`
+}
+
+func (c *Client) UpdatePlaylistMetadata(playlistID, name, overview string) error {
+	params := c.defaultParams()
+	body := updatePlaylistBody{
+		Name:     name,
+		Overview: overview,
+	}
+	resp, err := c.post(fmt.Sprintf("/Items/%s", playlistID), params, body)
+	if err != nil {
+		return fmt.Errorf("update playlist metadata: %v", err)
+	}
+	resp.Close()
+	return nil
+}
+
+func (c *Client) AddSongsToPlaylist(playlistID string, trackIDs []string) error {
+	params := c.defaultParams()
+	params["ids"] = strings.Join(trackIDs, ",")
+	resp, err := c.post(fmt.Sprintf("/Playlists/%s/Items", playlistID), params, struct{}{})
+	if err != nil {
+		return fmt.Errorf("add songs to playlist: %v", err)
+	}
+	resp.Close()
+	return nil
+}
+
+func (c *Client) RemoveSongsFromPlaylist(playlistID string, trackIDs []string) error {
+	params := c.defaultParams()
+	params["entryIds"] = strings.Join(trackIDs, ",")
+	resp, err := c.delete(fmt.Sprintf("/Playlists/%s/Items", playlistID), params)
+	if err != nil {
+		return fmt.Errorf("remove songs from playlist: %v", err)
+	}
+	resp.Close()
+	return nil
+}
+
+func (c *Client) MovePlaylistSong(playlistID string, trackID string, newIdx int) error {
+	endpoint := fmt.Sprintf("/Playlists/%s/Items/%s/Move/%d", playlistID, trackID, newIdx)
+	resp, err := c.post(endpoint, c.defaultParams(), struct{}{})
+	if err != nil {
+		return fmt.Errorf("move playlist song: %v", err)
+	}
+	resp.Close()
+	return nil
+
 }
 
 func (c *Client) DeletePlaylist(playlistID string) error {
