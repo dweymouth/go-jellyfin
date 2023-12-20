0cb10706d9440061f46ed59331cc54a9a1ad6f6f
freakytoad1 <admin@freakytoad1.tech>
2023-12-19 20:18:50 -0600

    makerequest cleanup

--- request.go
+++ request.go
@@ -124,7 +124,7 @@ func appendFilter(old, new string, separator string) string {
 }
 
 func (c *Client) get(url string, params params) (io.ReadCloser, error) {
-	resp, err := c.makeRequest(http.MethodGet, url, nil, params, nil)
+	resp, err := c.makeDo(http.MethodGet, url, nil, params, nil)
 	if resp != nil {
 		return resp.Body, err
 	}
@@ -132,7 +132,7 @@ func (c *Client) get(url string, params params) (io.ReadCloser, error) {
 }
 
 func (c *Client) delete(url string, params params) (io.ReadCloser, error) {
-	resp, err := c.makeRequest(http.MethodDelete, url, nil, params, nil)
+	resp, err := c.makeDo(http.MethodDelete, url, nil, params, nil)
 	if resp != nil {
 		return resp.Body, err
 	}
@@ -144,7 +144,7 @@ func (c *Client) post(url string, params params, body interface{}) (io.ReadClose
 	if err != nil {
 		return nil, fmt.Errorf("marshal POST body: %v", err)
 	}
-	resp, err := c.makeRequest(http.MethodPost, url, bodyEnc, params, nil)
+	resp, err := c.makeDo(http.MethodPost, url, bodyEnc, params, nil)
 	if resp != nil {
 		return resp.Body, err
 	}
@@ -167,34 +167,35 @@ func (c *Client) encodeGETUrl(endpoint string, params params) (string, error) {
 	return req.URL.String(), nil
 }
 
-// Construct request
-// Set authorization header and build url query
+// makeDo constructs request and performs Do.
+// Set authorization header and build url query.
 // Make request, parse response code and raise error if needed. Else return response body
-func (c *Client) makeRequest(method, url string, body []byte, params params, headers map[string]string) (*http.Response, error) {
-	var reader *bytes.Buffer
+func (c *Client) makeDo(method, url string, body []byte, params params, headers map[string]string) (*http.Response, error) {
 	var req *http.Request
 	var err error
+
+	u := fmt.Sprintf("%s%s", c.BaseURL(), url)
+
+	// generate http.Request
 	if body != nil {
-		reader = bytes.NewBuffer(body)
-		req, err = http.NewRequest(method, fmt.Sprintf("%s%s", c.BaseURL(), url), reader)
+		req, err = http.NewRequest(method, u, bytes.NewBuffer(body))
 	} else {
-		req, err = http.NewRequest(method, fmt.Sprintf("%s%s", c.BaseURL(), url), nil)
+		req, err = http.NewRequest(method, u, nil)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 
-	if err != nil {
-		return &http.Response{}, fmt.Errorf("failed to make request: %v", err)
-	}
+	// set headers
 	if method == http.MethodPost {
 		req.Header.Set("Content-Type", "application/json")
 	}
 	req.Header.Set("X-Emby-Token", c.token)
-
-	if len(headers) > 0 {
-		for k, v := range headers {
-			req.Header.Set(k, v)
-		}
+	for k, v := range headers {
+		req.Header.Set(k, v)
 	}
 
+	// set params
 	if params != nil {
 		q := req.URL.Query()
 		for i, v := range params {
@@ -202,23 +203,36 @@ func (c *Client) makeRequest(method, url string, body []byte, params params, hea
 		}
 		req.URL.RawQuery = q.Encode()
 	}
+
+	// DO
 	//start := time.Now()
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("failed make request: %v", err)
+		return nil, fmt.Errorf("failed to do request: %w", err)
 	}
 	//took := time.Since(start)
 	//logrus.Debugf("%s %s: %d (%d ms)", req.Method, req.URL.Path, resp.StatusCode, took.Milliseconds())
 
+	// check response for errors and return the response
+	return checkResponse(resp)
+}
+
+// checkResponse determines if there is was an error returned by jellyfin.
+func checkResponse(resp *http.Response) (*http.Response, error) {
+	// 200 or 204 is all good
 	if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusNoContent {
 		return resp, nil
 	}
+
+	// read in the body and look for an error message
 	bytes, _ := io.ReadAll(resp.Body)
-	var msg string = "no body"
+	msg := "no body"
 	if len(bytes) > 0 {
 		msg = string(bytes)
 	}
-	var errMsg string
+
+	errMsg := errUnexpectedStatusCode
+
 	switch resp.StatusCode {
 	case http.StatusBadRequest:
 		errMsg = errInvalidRequest
@@ -230,8 +244,7 @@ func (c *Client) makeRequest(method, url string, body []byte, params params, hea
 		errMsg = errNotFound
 	case http.StatusInternalServerError:
 		errMsg = errServerError
-	default:
-		errMsg = errUnexpectedStatusCode
 	}
-	return resp, fmt.Errorf("%s, code: %d, msg: %s", errMsg, resp.StatusCode, msg)
+
+	return resp, fmt.Errorf("%s, code: %s, msg: %s", errMsg, resp.Status, msg)
 }
